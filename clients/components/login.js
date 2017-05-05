d02dbd5d8198f6a5101d5b106b6583ebadb37227
Drewclam <andrew.c.lam94@gmail.com>
2017-05-05 00:30:56 -0400

    Add log-in directive and html page

---
+++ clients/components/login.js
@@ -0,0 +1,12 @@
+angular.module('hikingApp')
+
+.directive('login', () => {
+  return {
+    controller: () => {
+
+    },
+    controllerAs: 'ctrl',
+    bindToController: true
+    templateUrl: '../templates/login.html'
+  }
+})
