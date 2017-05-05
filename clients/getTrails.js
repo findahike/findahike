f0e9ca9db73ec48f0968ae9d66031d63da25567f
vicki alden <vic.alden23@gmail.com>
2017-05-05 10:42:24 -0700

    handle client side get

--- clients/getTrails.js
+++ clients/getTrails.js
@@ -1,18 +1,14 @@
-angular.module('hikingApp');
+angular.module('hikingApp', [])
   .controller('getTrailsCtrl', function($scope, $http) {
 
     $scope.searchTrailsApi = function(city) {
       console.log('THIS IS THE INPUT CITY', city);
       $http({
-        url: 'https://trailapi-trailapi.p.mashape.com/',
+        url: `https://trailapi-trailapi.p.mashape.com/?limit=25&q[activities_activity_type_name_eq]=hiking&q[city_cont]=${city}&radius=25` ,
         method: 'GET',
-        data: {
-          'q[activities_activity_type_name_eq]': 'hiking',
-          'q[city_cont]': city
-        }
         dataType: 'json',
         headers: {
-          "X-Mashape-Authorization", "9O9keAxJxLmshmdXyniXY16RuAKtp1h06Ldjsn6f7qf7dIfGQr"
+          "X-Mashape-Authorization": "By0afuBqQKmshwWHnoeEyP355rW1p1BjdbyjsnCIx9rkj4ljar",
           'Content-Type':'application/x-www-form-urlencoded'
         }
       }).then(function successCallback(response) {
