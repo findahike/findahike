31d0c086ff71c9d0afb5072470dae08c8cfcc28f
Drewclam <andrew.c.lam94@gmail.com>
2017-05-04 22:59:02 -0400

    Add some properties to directive and change comment in html

--- clients/components/takeAHike.js
+++ clients/components/takeAHike.js
@@ -3,6 +3,11 @@ angular.module('hikingApp')
 // Search hikes
 .directive('takeAHike', function() {
   return {
+    controller: function() {
+
+    },
+    bindToController: true,
+    controllerAs: 'ctrl'  /////////////////////////////// PAY ATTENTIOn
     templateUrl: '../templates/takeAHike.html'
   }
 });
