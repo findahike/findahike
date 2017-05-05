eafa432c9440cc35790897187ac6d8dd61261350
vicki alden <vic.alden23@gmail.com>
2017-05-05 09:16:07 -0700

    add html for getTrails

--- clients/getTrails.js
+++ clients/getTrails.js
@@ -1,4 +1,25 @@
-angular.module('hikingApp').service;
+angular.module('hikingApp');
   .controller('getTrailsCtrl', function($scope, $http) {
 
-  })
+    $scope.searchTrailsApi = function(city) {
+      console.log('THIS IS THE INPUT CITY', city);
+      $http({
+        url: 'https://trailapi-trailapi.p.mashape.com/',
+        method: 'GET',
+        data: {
+          'q[activities_activity_type_name_eq]': 'hiking',
+          'q[city_cont]': city
+        }
+        dataType: 'json',
+        headers: {
+          "X-Mashape-Authorization", "9O9keAxJxLmshmdXyniXY16RuAKtp1h06Ldjsn6f7qf7dIfGQr"
+          'Content-Type':'application/x-www-form-urlencoded'
+        }
+      }).then(function successCallback(response) {
+        console.log(response);
+      }, function errorCallback(response) {
+        console.log(response);
+      })
+    }
+
+  });
