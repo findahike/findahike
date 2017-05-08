cf380d452fd34adc4d5b62248f6e8da8b9d780c5
Drewclam <andrew.c.lam94@gmail.com>
2017-05-08 12:31:51 -0400

    Hook up hike services to appController
    TODO: separate controller into pieces as described in appController.js

--- clients/controllers/appController.js
+++ clients/controllers/appController.js
@@ -1,8 +1,13 @@
 angular.module('hikingApp')
-.controller('appController', ($scope) =>{
-  $scope.
-});
-
+.controller('appController', ($scope, getHikeApiService, getHikeService, postHikeService) =>{
+  $scope.searchTrailsApi = getHikeApiService.searchTrailsApi;
+  $scope.findCompletedHikes = getHikeService.findCompletedHikes;
+  $scope.findWishListHikes = getHikeService.findWishListHikes;
+  $scope.getTrailProfile = getHikeService.getTrailProfile;
+  $scope.saveCompletedHike = postHikeService.saveCompletedHike;
+  $scope.saveWishListHike = postHikeService.saveWishListHike;
+})
+// TODO: BREAK CONTROLLER INTO THESE PIECES:
 //Shared Functions
 //openHike - to open hike profile page - on user.html & takeAHike.html
 
