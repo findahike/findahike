db6f2b30e4d6571072d08a13d1a44dd9f33a8151
Drewclam <andrew.c.lam94@gmail.com>
2017-05-08 19:29:45 -0400

    Pass api data to callback in an array
    Delete duplicate html file

--- clients/services/getHikeApiService.js
+++ clients/services/getHikeApiService.js
@@ -1,20 +1,48 @@
 angular.module('hikingApp')
+
 .service('getHikeApiService', ($http, city) => {
-  this.searchTrailsApi = function(city) {
-    console.log('THIS IS THE INPUT CITY', city);
-      $http({
-        url: `https://trailapi-trailapi.p.mashape.com/?limit=25&q[activities_activity_type_name_eq]=hiking&q[city_cont]=${city}&radius=25` ,
-        method: 'GET',
-        dataType: 'json',
-        headers: {
+
+  this.searchTrailsApi = (city) => {
+
+    var apiParams = {
+      url ='https://trailapi-trailapi.p.mashape.com/',
+      limit = 'limit=25',
+      activity = 'q[activities_activity_type_name_eq]=hiking',
+      city = 'q[city_cont]=' + city,
+      radius = 'radius=25'
+    };
+
+    $http({
+      url: `${apiParams.url}?${apiParams.limit}&${apiParams.activity}&${apiParams.city}&${apiParams.radius}`,
+      method: 'GET',
+      dataType: 'json',
+      headers: {
         "X-Mashape-Authorization": "By0afuBqQKmshwWHnoeEyP355rW1p1BjdbyjsnCIx9rkj4ljar",
         'Content-Type':'application/x-www-form-urlencoded'
       }
-    }).success((response) => {
-      console.log(response, 'connected to trailApi');
     })
-    .error((status, response) => {
-      console.log(status, 'error connecting to trailApi');
+    .then((results) => {
+      console.log('successfully connected to trails api', results);
+      var hikes = [];
+
+      results.places.forEach((entry) => { // extract data of interest
+        var hike = {};
+        hike.directions = entry.directions;
+        hike.lat = entry.lat;
+        hike.lon = entry.long;
+        entry.activites.forEach((activity) => {
+          hike.name = activity.name;
+          hike.description = activity.description;
+          hike.url = activity.url;
+        });
+        hikes.push(hike);
+      });
+
+      callback(hikes);
+    })
+    .catch((err) => {
+      console.log('error connecting to trails api', err);
     });
   };
-});
+
+})
