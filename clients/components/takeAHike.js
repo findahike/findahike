0e5df26ba588a0c86e66e0e0407d033eecf22cdd
Robert Si <robertsi0214@gmail.com>
2017-05-09 16:34:32 -0700

    almost ocmpleted database

--- clients/components/takeAHike.js
+++ clients/components/takeAHike.js
@@ -2,24 +2,24 @@ angular.module('hikingApp')
 
 // Search hikes
 .directive('takeAHike', () => {
-  return {
-    restrict: 'E',
-    scope: {
-      trails: '<',
-      onClick: '<'
-    },
-    controller: () => {
+ return {
+   restrict: 'E',
+   scope: {
+     trails: '<',
+     onClick: '<'
+   },
+   controller: () => {
 
-      //runs through trail array and sorts each object by the search value.
-      //when we call the function on the click, we will need to pass in both parameters
-      $scope.filter = (trails, searchVal) => {
-        return trails.sort(function(obj1, obj2){
-          return obj1[searchVal] - obj2[searchVal];
-        });
-      };
-    },
-    controllerAs: 'ctrl',
-    bindToController: true,
-    templateUrl: '../templates/takeAHike.html'
-  };
+     //runs through trail array and sorts each object by the search value.
+     //when we call the function on the click, we will need to pass in both parameters
+     $scope.filter = (trails, searchVal) => {
+       return trails.sort(function(obj1, obj2){
+         return obj1[searchVal] - obj2[searchVal];
+       });
+     };
+   },
+   controllerAs: 'ctrl',
+   bindToController: true,
+   templateUrl: '../templates/takeAHike.html'
+ };
 });
