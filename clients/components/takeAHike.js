01e6dbc1a6bb18ef842d31bc103379f47d294ec7
Drewclam <andrew.c.lam94@gmail.com>
2017-05-10 13:45:32 -0400

    Incorporate api service with takeAHike.html on click
    Fix some syntax in api service

--- clients/components/takeAHike.js
+++ clients/components/takeAHike.js
@@ -12,7 +12,7 @@ angular.module('hikingApp')
 
      //runs through trail array and sorts each object by the search value.
      //when we call the function on the click, we will need to pass in both parameters
-     $scope.filter = (trails, searchVal) => {
+     this.filter = (trails, searchVal) => {
        return trails.sort(function(obj1, obj2){
          return obj1[searchVal] - obj2[searchVal];
        });
