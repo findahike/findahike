87f19a9c953799fe093b0a2cef0ce2024e45e016
vicki alden <vic.alden23@gmail.com>
2017-05-10 20:48:42 -0700

    render hike profile on click event

--- clients/components/hikeProfile.js
+++ clients/components/hikeProfile.js
@@ -2,6 +2,15 @@ angular.module('hikingApp')
 
 .directive('hikeProfile', () => {
   return {
+    restrict: 'E',
+    scope: {
+      changeViewState: '<'
+    },
+    controller: ($scope) => {
+      //console.log($scope);
+    },
+    controllerAs: 'takeAHike',
+    bindToController: true,
     templateUrl: 'clients/templates/hikeProfile.html'
   };
 });
