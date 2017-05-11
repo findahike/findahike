cb2eb46f5d63e5efe692245bdead12536521ea84
vicki alden <vic.alden23@gmail.com>
2017-05-10 20:48:42 -0700

    render hike profile on click event

--- clients/components/takeAHike.js
+++ clients/components/takeAHike.js
@@ -2,27 +2,28 @@ angular.module('hikingApp')
 
 // Search hikes
 .directive('takeAHike', () => {
- return {
-   restrict: 'E',
-   scope: {
-     searchTrailsApi: '<',
-     searchResults: '<',
-     filter: '&'
-   },
-   controller: ($scope) => {
+  return {
+    restrict: 'E',
+    scope: {
+      changeViewState: '<',
+      searchTrailsApi: '<',
+      searchResults: '<',
+      filter: '&'
+    },
+    controller: ($scope) => {
 
-    console.log($scope);
+      console.log($scope);
 
      //runs through trail array and sorts each object by the search value.
      //when we call the function on the click, we will need to pass in both parameters
-     $scope.filter = (searchResults, searchVal) => {
-       return searchResults.sort(function(obj1, obj2){
-         return obj1[searchVal] - obj2[searchVal];
-       });
-     };
-   },
-   controllerAs: 'takeAHike',
-   bindToController: true,
-   templateUrl: 'clients/templates/takeAHike.html'
- };
+      $scope.filter = (searchResults, searchVal) => {
+        return searchResults.sort(function(obj1, obj2){
+          return obj1[searchVal] - obj2[searchVal];
+        });
+      };
+    },
+    controllerAs: 'takeAHike',
+    bindToController: true,
+    templateUrl: 'clients/templates/takeAHike.html'
+  };
 });
